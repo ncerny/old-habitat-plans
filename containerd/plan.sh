c13a52aab605bff140f0362b55a30802e6d244ac
Nathan Cerny <ncerny@gmail.com>
2018-04-08 15:52:09 -0500

    Fix race condition in cfssl, fix containerd, fix etcd31, start kubelet.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- containerd/plan.sh
+++ containerd/plan.sh
@@ -2,21 +2,19 @@ pkg_name=containerd
 pkg_description="containerd is an industry-standard container runtime with an emphasis on simplicity, robustness and portability."
 pkg_upstream_url="https://github.com/containerd/containerd"
 pkg_origin=ncerny
-pkg_version="1.0.2"
+pkg_version="1.1.0-rc.1"
 pkg_maintainer="Nathan Cerny <ncerny@gmail.com>"
 pkg_license=("Apache-2.0")
 pkg_source="https://github.com/containerd/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.linux-amd64.tar.gz"
-pkg_shasum="4a15ad04f2bea497454044f05e792d9ec51deb40829e8f97d4519838605e7cf4"
+pkg_shasum="523be2d779b1337bdc7b584f810f29fc579c175d9123cd4d0368ec8e3e05ca0a"
 pkg_dirname="bin"
-pkg_deps=(core/glibc core/bash core/kmod)
+pkg_deps=(core/glibc core/kmod)
 pkg_build_deps=()
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
 pkg_svc_user=root
 pkg_svc_group=${pkg_svc_user}
-
-
 
 do_unpack() {
     unpack_file $pkg_filename
